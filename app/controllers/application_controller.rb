f0dedbc87297e74b1d10bec01fba4160ab3a0515
Raja & yoosters cobite <pair+raja-sharma.yoosters@devbootcamp.com>
2017-07-24 16:09:35 -0400

    Work in progress on merchant show page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    if session[:type] == 'merchant'
+      current_user||=Merchant.find_by(id: session[:user_id])
+    elsif session[:type] == 'contractor'
+      current_user||=Contractor.find_by(id: session[:user_id])
+    else
+      false
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  helper_method :current_user
+  helper_method :logged_in?
 end
--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,26 +1,7 @@
 class MerchantsController < ApplicationController
 
-  def new
-    @merchant = Merchant.new
-  end
-
-  def create
-    @merchant = Merchant.new(merchant_params)
-    if @merchant.save
-      redirect_to @merhcant
-    else
-      @errors = @merchant.errors.full_messages
-      render 'new'
-    end
-  end
-
   def show
     @merchant = Merchant.find(params[:id])
   end
-
-  private
-    def merchant_params
-      params.require(:merchant).permit(:name, :address, :email, :password)
-    end
 
 end
