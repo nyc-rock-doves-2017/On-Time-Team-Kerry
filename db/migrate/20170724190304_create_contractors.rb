b6ad31a69db3f0794de19f63f55b91731622043c
Iftekhar Alam <email": null>
2017-07-24 16:19:35 -0400

    Sleight update in models to get secure password

--- db/migrate/20170724190304_create_contractors.rb
+++ db/migrate/20170724190304_create_contractors.rb
@@ -2,8 +2,9 @@ class CreateContractors < ActiveRecord::Migration[5.1]
   def change
     create_table :contractors do |t|
 
-      t.string :name, :email, null: false, uniqueness: true
-      t.string :password, null: false
+      t.string :name, null: false
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
       t.boolean :status, true
 
       t.timestamps
