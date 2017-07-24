236b42ea2a716287fec8567e4a51b25090ccdc54
Evans Wang & Kelsey Malone <pair+evansjwang.kqm001@devbootcamp.com>
2017-07-24 16:06:42 -0400

    Create Login and Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'sessions#home'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'sessions#newsignup'
+  post '/signup' => 'sessions#createsignup'
+  get '/registration' => 'sessions#newregistration'
+  post '/registration' => 'sessions#createregistration'
+
 end
