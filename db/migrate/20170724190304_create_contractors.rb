7c53d4601aa9b8f5b5a30cdc580b356f993291e5
Iftekhar Alam <email": null>
2017-07-24 17:18:38 -0400

    Update migrations and create some seeds

--- db/migrate/20170724190304_create_contractors.rb
+++ db/migrate/20170724190304_create_contractors.rb
@@ -5,9 +5,12 @@ class CreateContractors < ActiveRecord::Migration[5.1]
       t.string :name, null: false
       t.string :email, null: false, unique: true
       t.string :password_digest, null: false
-      t.boolean :status, true
+      t.boolean :status
 
       t.timestamps
     end
+  end
+  def up
+    change_column_default :contractors, :status, true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,17 @@ ActiveRecord::Schema.define(version: 20170724190304) do
   create_table "contractors", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
-    t.string "password", null: false
+    t.string "password_digest", null: false
     t.boolean "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "merchants", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
