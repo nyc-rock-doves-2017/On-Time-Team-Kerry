ebc03e5b26ebbef03215452e85d997b69f9c74d4
Kelsey Malone <kelseymalone@Kelseys-MacBook-Pro.local>
2017-07-24 23:22:52 -0400

    Update merchant dashboard

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,32 @@
 class Order < ApplicationRecord
   belongs_to :merchant
   belongs_to :contractor
+
+  def claim_to_pick_up_time
+    if self.pick_up_time && self.claim_time
+      diff = self.pick_up_time - self.claim_time
+      (diff/60).floor
+    else
+      "---"
+    end
+  end
+
+  def pick_up_to_delivery_time
+    if self.delivery_time  && self.pick_up_time
+      diff = self.delivery_time - self.pick_up_time
+      (diff/60).floor
+    else
+      "---"
+    end
+  end
+
+  def claim_to_delivery_time
+    if self.delivery_time && self.claim_time
+      diff = self.delivery_time - self.claim_time
+      (diff/60).floor
+    else
+      "---"
+    end
+
+  end
 end
