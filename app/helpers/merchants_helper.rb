1b3622c3c9f95e9aa40a92fb0916549c6ebcd526
Evans Wang & Kelsey Malone <pair+evansjwang.kqm001@devbootcamp.com>
2017-07-25 09:43:44 -0400

    Format dashboard

--- app/helpers/merchants_helper.rb
+++ app/helpers/merchants_helper.rb
@@ -2,40 +2,39 @@ module MerchantsHelper
 
   def average_claim_to_pick_up_time
     total_diff = 0
-    num_of_orders = @merchant.orders.length
-    # this is not working  - trying to account for edge case where length is greater than 0 - this is not working because order is not in scope outside of do block
-    if num_of_orders != 0 && order.claim_time && order.pick_up_time
-      @merchant.orders.each do |order|
+    com_orders = @merchant.orders.select { |o| o.delivery_time != nil }
+    if com_orders.length != 0
+      com_orders.each do |order|
         diff = order.pick_up_time - order.claim_time
         total_diff += diff
       end
-      average_diff = total_diff / num_of_orders
+      average_diff = total_diff / com_orders.length
       (average_diff/60).floor
     end
   end
 
   def average_pick_up_to_delivery_time
     total_diff = 0
-    num_of_orders = @merchant.orders.length
-      if num_of_orders != 0 && order.pick_up_time && order.delivery_time
-        @merchant.orders.each do |order|
+    com_orders = @merchant.orders.select { |o| o.delivery_time != nil }
+    if com_orders.length != 0
+      com_orders.each do |order|
           diff = order.delivery_time - order.pick_up_time
           total_diff += diff
         end
-        average_diff = total_diff / num_of_orders
+        average_diff = total_diff / com_orders.length
         (average_diff/60).floor
       end
   end
 
   def average_claim_to_delivery_time
     total_diff = 0
-    num_of_orders = @merchant.orders.length
-      if num_of_orders != 0 && order.claim_time && order.delivery_time
-        @merchant.orders.each do |order|
+    com_orders = @merchant.orders.select { |o| o.delivery_time != nil }
+    if com_orders.length != 0
+      com_orders.each do |order|
           diff = order.delivery_time - order.claim_time
           total_diff += diff
         end
-        average_diff = total_diff / num_of_orders
+        average_diff = total_diff / com_orders.length
         (average_diff/60).floor
       end
   end
